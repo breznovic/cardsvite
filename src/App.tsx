ee48414e1c26936664fc42d77cba2b3d418131e6
breznovic <slymarkel@gmail.com>
2022-01-16 16:52:27 +0300

    Header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from'./app.module.css'
+import Header from "./Header/Header"
+
+function App() {
+    return (
+        <div className={classes.app}>
+            <Header/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './header.module.css';
+
+function App() {
+    return (
+        <div className={classes.header}>
+
+        </div>
+    );
+}
+
+export default App;
