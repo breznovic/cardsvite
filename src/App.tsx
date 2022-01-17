a9f73ae6319011d5cf6fed1367bec48046bce737
breznovic <slymarkel@gmail.com>
2022-01-17 10:46:23 +0300

    Scratch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import classes from'./app.module.css'
 import Header from "./Header/Header"
+import Main from "./Main/Main";
 
 function App() {
     return (
         <div className={classes.app}>
             <Header/>
+            <Main/>
         </div>
     )
 }
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './header.module.css';
+import Nav from "../Nav/Nav";
 
-function App() {
+function Header() {
     return (
         <div className={classes.header}>
-
+            <Nav/>
         </div>
     );
 }
 
-export default App;
+export default Header
