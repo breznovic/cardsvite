840787a7bee35b251aabc58acbb1af5818c5b2a6
breznovic <slymarkel@gmail.com>
2022-07-22 14:14:40 +0300

    Scratch

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react'
-import classes from './app.module.css'
-import Navbar from "./components/Navbar";
-import {Grid} from "@mui/material";
-import Leftbar from "./components/Leftbar";
-import Feed from "./components/Feed";
-import Rightbar from "./components/Rightbar";
+import DashBoard from './components/DashBoard/DashBoard';
 
 function App() {
 
     return (
-        <div className={classes.app}>
-            <div><Navbar/>
-                <Grid container>
-                    <Grid item sm={2}><Leftbar/></Grid>
-                    <Grid item sm={7}><Feed/></Grid >
-                    <Grid item sm={3}><Rightbar/></Grid>
-                </Grid>
-            </div>
+        <div>
+           <DashBoard/>
         </div>
     )
 }
---
+++ src/components/DashBoard/DashBoard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DashBoard = () => {
+    return (
+        <div>
+            Paella
+            Taxi
+            Flight
+            Apartment
+            Weather
+            Photo
+        </div>
+    )
+}
+
+export default DashBoard
