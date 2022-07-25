e7d8824c4a005d7c57877f72b36e54fd09e3dd9e
breznovic <slymarkel@gmail.com>
2022-07-25 17:38:58 +0300

    Paella price

--- src/components/DashBoard/DashBoard.tsx
+++ src/components/DashBoard/DashBoard.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {ImageGallery} from '../Photo/ImageGallery';
 import Paella from "../Paella/Paella";
+import s from '../DashBoard/DashBoard.module.css'
 
 const DashBoard = () => {
 
     return (
-        <div>
+        <div className={s.position}>
             <Paella/>
             Taxi
             Flight
--- src/components/Paella/Paella.tsx
+++ src/components/Paella/Paella.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from 'react'
+import {v1} from 'uuid'
+import s from '../Paella/Paella.module.css'
 
 const Paella = () => {
 
-    let ingridients = [
-        'Onion',
-        'Bell pepper',
-        'Garlic',
-        'Tomatoes',
-        'Parsley',
-        'Frozen peas',
-        'Bay leaf',
-        'Paprika',
-        'Saffron',
-        'Salt',
-        'Pepper',
-        'Shrimp',
-        'Mussels',
-        'Calamari',
-        'Olive oil',
-        'White wine',
-        'Rice'
+    let ingredients = [
+        {id: v1(), ingredient: 'Onion', price: 1},
+        {id: v1(), ingredient: 'Bell pepper', price: 1},
+        {id: v1(), ingredient: 'Tomatoes', price: 1},
+        {id: v1(), ingredient: 'Parsley', price: 1},
+        {id: v1(), ingredient: 'Frozen peas', price: 1},
+        {id: v1(), ingredient: 'Bay leaf', price: 1},
+        {id: v1(), ingredient: 'Paprika', price: 1},
+        {id: v1(), ingredient: 'Saffron', price: 1},
+        {id: v1(), ingredient: 'Salt', price: 1},
+        {id: v1(), ingredient: 'Pepper', price: 1},
+        {id: v1(), ingredient: 'Shrimp', price: 1},
+        {id: v1(), ingredient: 'Mussels', price: 1},
+        {id: v1(), ingredient: 'Calamari', price: 1},
+        {id: v1(), ingredient: 'Olive oil', price: 1},
+        {id: v1(), ingredient: 'White wine', price: 1},
+        {id: v1(), ingredient: 'Rice', price: 1},
     ]
 
-    let recipe = ingridients.map((i) => {
-        return <ol>
-            {i}
-        </ol>
-    })
+    let paellaPrice = ingredients.map(i => i.price).reduce((prev, curr) => prev + curr, 0)
 
     return (
         <div>
-            {recipe}
+            <img
+                src='https://images.aws.nestle.recipes/resized/efc1f7093ccb8a17d90b4f33d203a732_image-1_1500x700_708_600.jpg'
+                className={s.paella}/>
+            <div>Paella price: {paellaPrice} euro</div>
         </div>
     )
 }
