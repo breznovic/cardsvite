371b895d19b57b316f15e9f0b0b8d4d92ef5f720
breznovic <slymarkel@gmail.com>
2022-10-26 20:20:18 +0300

    New

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import s from "./App.module.css"
-import Paella from "./components/Paella/Paella"
-import Apartment from "./components/Apartment/Apartment"
-import Flight from "./components/Flight/Flight"
-import Weather from "./components/Weather/Weather"
-import {ImageGallery} from "./components/Photo/ImageGallery"
+
 
 function App() {
 
-    return <div className={s.image}>
-            <div className={s.position}>
-                <div className={s.element}><Paella/></div>
-                <div className={s.element}><Apartment/></div>
-                <div className={s.element}><Flight/></div>
-                <div className={s.element}><Weather/></div>
-                <div className={s.element}><ImageGallery/></div>
-            </div>
+    return <div>
+
         </div>
 }
 
