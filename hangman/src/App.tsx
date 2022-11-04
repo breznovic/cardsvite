32d4f74481e6ae96e8fa91240cbfed5aba316f6c
breznovic <slymarkel@gmail.com>
2022-11-04 15:14:09 +0300

    Guess

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,5 +1,5 @@
 import words from './wordList.json'
-import {useState} from 'react'
+import {useCallback, useState} from 'react'
 
 function getWord() {
     return words[Math.floor(Math.random() * words.length)]
@@ -8,10 +8,30 @@ function getWord() {
 function App() {
 
     const [wordToGuess, setWordToGuess] = useState(getWord)
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+    const incorrectLetters = guessedLetters.filter(
+        letter => !wordToGuess.includes(letter)
+    )
+
+    const isLoser = incorrectLetters.length >= 6
+    const isWinner = wordToGuess
+        .split("")
+        .every(letter => guessedLetters.includes(letter))
+
+    const addGuessedLetter = useCallback(
+        (letter: string) => {
+            if (guessedLetters.includes(letter) || isLoser || isWinner) return
+
+            setGuessedLetters(currentLetters => [...currentLetters, letter])
+        },
+        [guessedLetters, isWinner, isLoser]
+    )
+
 
     return (
         <div>
-            еуые
+            Test
         </div>
     )
 }
