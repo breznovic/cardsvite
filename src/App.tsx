c940734208e0bcf0fec5336b65c852dced10a2f7
breznovic <slymarkel@gmail.com>
2024-02-14 17:31:38 +0300

    fix Slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import { Slider } from "./components/ui/Slider/Slider";
-
-
 export function App() {
-  return (
-    <div>
-      <Slider min={2} max={10}/>
-    </div>
-  )
+  return <div>Test</div>
 }
