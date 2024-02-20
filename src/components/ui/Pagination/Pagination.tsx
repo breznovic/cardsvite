a670aa287be7dc89de0d4c21cbf7db1132ab28b2
breznovic <slymarkel@gmail.com>
2024-02-20 19:55:26 +0300

    feat: add Pagination draft

---
+++ src/components/ui/Pagination/Pagination.tsx
@@ -0,0 +1,49 @@
+import s from './Pagination.module.scss'
+import { usePagination } from './usePagination'
+import { Pages } from './Pages'
+import { Button } from '../Button/button'
+
+
+type Props = {
+  currentPage: number
+  itemsPerPage: number
+  onPageChange: (page: number) => void
+  totalItems?: number
+  totalPages?: number
+}
+export const Pagination = ({
+  currentPage,
+  itemsPerPage,
+  onPageChange,
+  totalItems = 0,
+  totalPages = 0,
+}: Props) => {
+  const onBackClickHandler = () => onPageChange(currentPage - 1)
+  const onForwardClickHandler = () => onPageChange(currentPage + 1)
+
+  const pages = usePagination({
+    currentPage,
+    pageSize: itemsPerPage,
+    siblingCount: 1,
+    totalCount: totalItems,
+    totalPageCount: totalPages,
+  })
+
+  const items = pages()
+
+  return (
+    <div className={s.container}>
+      <Button className={s.button} disabled={currentPage <= 1} onClick={onBackClickHandler}>
+        {'<'}
+      </Button>
+      <Pages currentPage={currentPage} items={items} onPageChange={onPageChange} />
+      <Button
+        className={s.button}
+        disabled={currentPage >= totalPages}
+        onClick={onForwardClickHandler}
+      >
+        {'>'}
+      </Button>
+    </div>
+  )
+}
